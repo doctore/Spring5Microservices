4f12c1a1b285889c65745bdc1f8d4c16967cb29a
doctore <c.javier.reyes.e@gmail.com>
2019-02-10 19:16:21 +0100

    Added alternative to JPA in memory pagination

--- pizza-service/src/main/java/com/pizza/configuration/Constants.java
+++ pizza-service/src/main/java/com/pizza/configuration/Constants.java
@@ -8,4 +8,7 @@ public class Constants {
     // Database schema on which the entities have been included
     public static final String DATABASE_SCHEMA = "eat";
 
+    // Path in the application on which the repositories are included
+    public static final String REPOSITORY_PATH = "com.pizza.repository";
+
 }
