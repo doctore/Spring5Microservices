8a35a4f1887dee38ddae9e2cd17e484597406533
doctore <c.javier.reyes.e@gmail.com>
2019-02-13 19:37:44 +0100

    Added BaseConverter to manage all conversions developed using MapStruct

--- pizza-service/src/main/java/com/pizza/util/converter/IngredientConverter.java
+++ pizza-service/src/main/java/com/pizza/util/converter/IngredientConverter.java
@@ -4,5 +4,8 @@ import com.pizza.dto.IngredientDto;
 import com.pizza.model.Ingredient;
 import org.mapstruct.Mapper;
 
+/**
+ * Utility class to convert from {@link Ingredient} to {@link IngredientDto} and vice versa.
+ */
 @Mapper
 public interface IngredientConverter extends BaseConverter<Ingredient, IngredientDto> {}
--- pizza-service/src/main/java/com/pizza/util/converter/PizzaConverter.java
+++ pizza-service/src/main/java/com/pizza/util/converter/PizzaConverter.java
@@ -4,5 +4,8 @@ import com.pizza.dto.PizzaDto;
 import com.pizza.model.Pizza;
 import org.mapstruct.Mapper;
 
+/**
+ * Utility class to convert from {@link Pizza} to {@link PizzaDto} and vice versa.
+ */
 @Mapper(uses={IngredientConverter.class})
 public interface PizzaConverter extends BaseConverter<Pizza, PizzaDto> {}
