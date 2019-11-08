31ab2790752f43bd94877fc459b52aea4550970a
doctore <c.javier.reyes.e@gmail.com>
2019-11-08 19:36:22 +0100

    Updated main route to security-swt-service

--- security-jwt-service/src/main/java/com/security/jwt/configuration/rest/RestRoutes.java
+++ security-jwt-service/src/main/java/com/security/jwt/configuration/rest/RestRoutes.java
@@ -6,7 +6,7 @@ package com.security.jwt.configuration.rest;
 public final class RestRoutes {
 
     public static final class SECURITY {
-        public static final String ROOT = "/security";
+        public static final String ROOT = "/security/jwt";
         public static final String AUTHORIZATION_INFO = "/authinfo";
         public static final String LOGIN = "/login";
         public static final String REFRESH = "/refresh";
