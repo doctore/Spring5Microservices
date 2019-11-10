fb2e9a1c5380dfbaec547371bf6a7e26ac729ae8
doctore <c.javier.reyes.e@gmail.com>
2019-11-10 18:00:37 +0100

    Removed old authentication-service

--- authentication-service/src/main/java/com/authenticationservice/enums/RoleEnum.java
+++
@@ -1,6 +0,0 @@
-package com.authenticationservice.enums;
-
-public enum RoleEnum {
-    ADMIN,
-    USER
-}
--- authentication-service/src/main/java/com/authenticationservice/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.authenticationservice.repository;
-
-import com.authenticationservice.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository  extends JpaRepository<Role, Integer> {}
