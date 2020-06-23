55442d8c32c9de77510ad52ace45d69cb3f9aee8
doctore <c.javier.reyes.e@gmail.com>
2020-06-23 19:00:05 +0200

    Added generic method to collect an specific property value of a given collection

--- common/src/main/java/com/spring5microservices/common/util/CollectionUtil.java
+++ common/src/main/java/com/spring5microservices/common/util/CollectionUtil.java
@@ -6,13 +6,60 @@ import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
+import static java.util.Arrays.asList;
 import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toCollection;
+import static java.util.stream.Collectors.toList;
 
 @UtilityClass
 public class CollectionUtil {
+
+    /**
+     * Return a {@link Collection} with the extracted property of the given {@code collection}
+     *
+     * @param collection
+     *    Source {@link Collection} with the property to extract
+     * @param keyExtractor
+     *    {@link Function} used to get the key we want to use to include in returned {@link Collection}
+     *
+     * @return {@link Collection}
+     */
+    public static <T, E> Collection<E> collectProperty(final Collection<T> collection, Function<? super T, E> keyExtractor) {
+        return collectProperty(collection, keyExtractor, null);
+    }
+
+
+    /**
+     * Return a {@link Collection} with the extracted property of the given {@code collection}
+     *
+     * @param collection
+     *    Source {@link Collection} with the property to extract
+     * @param keyExtractor
+     *    {@link Function} used to get the key we want to use to include in returned {@link Collection}
+     * @param collectionFactory
+     *    {@link Supplier} of the {@link Collection} used to store the returned elements.
+     *
+     * @return {@link Collection}
+     */
+    public static <T, E> Collection<E> collectProperty(final Collection<T> collection, Function<? super T, E> keyExtractor,
+                                                       Supplier<Collection<E>> collectionFactory) {
+        return ofNullable(collection)
+                .map(c -> {
+                    if (null == keyExtractor) {
+                        return null;
+                    }
+                    Stream<E> keyExtractedStream = collection.stream().map(keyExtractor);
+                    return null == collectionFactory
+                            ? keyExtractedStream.collect(toList())
+                            : keyExtractedStream.collect(toCollection(collectionFactory));
+                })
+                .orElse(null == collectionFactory ? asList() : collectionFactory.get());
+    }
+
 
     /**
      * Return the unique elements of the given {@link Collection}s.
