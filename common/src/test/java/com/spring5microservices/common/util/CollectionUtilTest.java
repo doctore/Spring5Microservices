67df95129fa7cc06a49b9b1ac66e6aab4bbb2a2d
doctore <c.javier.reyes.e@gmail.com>
2020-06-23 19:03:00 +0200

    Added generic method to collect an specific property value of a given collection

--- common/src/test/java/com/spring5microservices/common/util/CollectionUtilTest.java
+++ common/src/test/java/com/spring5microservices/common/util/CollectionUtilTest.java
@@ -1,19 +1,76 @@
 package com.spring5microservices.common.util;
 
+import com.spring5microservices.common.PizzaDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CollectionUtilTest {
+
+    static Stream<Arguments> collectPropertyNoCollectionFactoryTestCases() {
+        PizzaDto carbonaraCheap = new PizzaDto("Carbonara", 5D);
+        PizzaDto carbonaraExpense = new PizzaDto("Carbonara", 10D);
+        Function<PizzaDto, String> getName = PizzaDto::getName;
+        Function<PizzaDto, Double> getCost = PizzaDto::getCost;
+        return Stream.of(
+                //@formatter:off
+                //            collection,                                   keyExtractor,   expectedResult
+                Arguments.of( null,                                         null,           asList() ),
+                Arguments.of( asList(carbonaraCheap, carbonaraExpense),     getName,        asList(carbonaraCheap.getName(), carbonaraExpense.getName()) ),
+                Arguments.of( asList(carbonaraCheap, carbonaraExpense),     getCost,        asList(carbonaraCheap.getCost(), carbonaraExpense.getCost()) )
+        ); //@formatter:on
+    }
+
+    @ParameterizedTest
+    @MethodSource("collectPropertyNoCollectionFactoryTestCases")
+    @DisplayName("collectProperty: without collection factory all parameters test cases")
+    public void collectPropertyNoCollectionFactory_testCases(List<PizzaDto> collection, Function<PizzaDto, String> keyExtractor,
+                                                             Collection<String> expectedResult) {
+        Collection<String> result = CollectionUtil.collectProperty(collection, keyExtractor);
+        assertEquals(expectedResult, result);
+    }
+
+
+    static Stream<Arguments> collectPropertyAllParametersTestCases() {
+        PizzaDto carbonaraCheap = new PizzaDto("Carbonara", 5D);
+        PizzaDto carbonaraExpense = new PizzaDto("Carbonara", 10D);
+        Function<PizzaDto, String> getName = PizzaDto::getName;
+        Function<PizzaDto, Double> getCost = PizzaDto::getCost;
+        Supplier<Collection<String>> setSupplier = LinkedHashSet::new;
+        return Stream.of(
+                //@formatter:off
+                //            collection,                                   keyExtractor,   collectionFactory,   expectedResult
+                Arguments.of( null,                                         null,           null,                asList() ),
+                Arguments.of( null,                                         null,           setSupplier,         new HashSet<>() ),
+                Arguments.of( asList(carbonaraCheap, carbonaraExpense),     getName,        null,                asList(carbonaraCheap.getName(), carbonaraExpense.getName()) ),
+                Arguments.of( asList(carbonaraCheap, carbonaraExpense),     getName,        setSupplier,         Set.of(carbonaraCheap.getName()) ),
+                Arguments.of( asList(carbonaraCheap, carbonaraExpense),     getCost,        null,                asList(carbonaraCheap.getCost(), carbonaraExpense.getCost()) ),
+                Arguments.of( asList(carbonaraCheap, carbonaraExpense),     getCost,        setSupplier,         Set.of(carbonaraCheap.getCost(), carbonaraExpense.getCost()) )
+        ); //@formatter:on
+    }
+
+    @ParameterizedTest
+    @MethodSource("collectPropertyAllParametersTestCases")
+    @DisplayName("collectProperty: with all parameters test cases")
+    public void collectPropertyAllParameters_testCases(List<PizzaDto> collection, Function<PizzaDto, String> keyExtractor,
+                                                       Supplier<Collection<String>> collectionFactory, Collection<String> expectedResult) {
+        Collection<String> result = CollectionUtil.collectProperty(collection, keyExtractor, collectionFactory);
+        assertEquals(expectedResult, result);
+    }
+
 
     static Stream<Arguments> concatUniqueElementsTestCases() {
         return Stream.of(
