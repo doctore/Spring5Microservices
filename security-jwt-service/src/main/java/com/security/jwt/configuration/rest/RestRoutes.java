5538a7781a9e13289673b5054fb8afb39acbe48e
doctore <c.javier.reyes.e@gmail.com>
2022-07-10 20:41:07 +0200

    Minor improvements in RestRoutes

--- security-jwt-service/src/main/java/com/security/jwt/configuration/rest/RestRoutes.java
+++ security-jwt-service/src/main/java/com/security/jwt/configuration/rest/RestRoutes.java
@@ -4,6 +4,7 @@ package com.security.jwt.configuration.rest;
  * Used to define the REST API routes included in the project
  */
 public final class RestRoutes {
+
     public static final String ROOT = "/security/jwt";
 
     public static final class CACHE {
