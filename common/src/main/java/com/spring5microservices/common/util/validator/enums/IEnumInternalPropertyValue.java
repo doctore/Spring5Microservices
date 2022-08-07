b35f42e5f6597678aa8491e22eacc0db4d1dd9b7
doctore <c.javier.reyes.e@gmail.com>
2022-08-07 11:25:50 +0200

    Moved validator enum functionality

---
+++ common/src/main/java/com/spring5microservices/common/util/validator/enums/IEnumInternalPropertyValue.java
@@ -0,0 +1,12 @@
+package com.spring5microservices.common.util.validator.enums;
+
+/**
+ * Used to get the value of an internal property in an {@link Enum}.
+ */
+public interface IEnumInternalPropertyValue<T> {
+
+    /**
+     * Get the value of an internal property included in the {@link Enum}.
+     */
+    T getInternalPropertyValue();
+}
