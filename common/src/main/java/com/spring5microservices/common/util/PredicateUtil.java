8822336a880303586b17c2a3c1c557fb9d966ef6
doctore <c.javier.reyes.e@gmail.com>
2023-01-01 19:42:32 +0100

    Added new method in PredicateUtil to return always true and false predicates

--- common/src/main/java/com/spring5microservices/common/util/PredicateUtil.java
+++ common/src/main/java/com/spring5microservices/common/util/PredicateUtil.java
@@ -5,11 +5,52 @@ import lombok.experimental.UtilityClass;
 import java.util.Collection;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiPredicate;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 @UtilityClass
 public class PredicateUtil {
+
+    /**
+     * Returns a {@link Predicate} with {@code false} as result.
+     *
+     * @return {@link Predicate}
+     */
+    public static <T> Predicate<T> alwaysFalse() {
+        return t -> false;
+    }
+
+
+    /**
+     * Returns a {@link Predicate} with {@code true} as result.
+     *
+     * @return {@link Predicate}
+     */
+    public static <T> Predicate<T> alwaysTrue() {
+        return t -> true;
+    }
+
+
+    /**
+     * Returns a {@link BiPredicate} with {@code false} as result.
+     *
+     * @return {@link BiPredicate}
+     */
+    public static <T, E> BiPredicate<T, E> biAlwaysFalse() {
+        return (t, e) -> false;
+    }
+
+
+    /**
+     * Returns a {@link BiPredicate} with {@code true} as result.
+     *
+     * @return {@link BiPredicate}
+     */
+    public static <T, E> BiPredicate<T, E> biAlwaysTrue() {
+        return (t, e) -> true;
+    }
+
 
     /**
      * Used when we want to get the unique elements of a given {@link Collection} by a specific property of its objects
--- common/src/test/java/com/spring5microservices/common/util/PredicateUtilTest.java
+++ common/src/test/java/com/spring5microservices/common/util/PredicateUtilTest.java
@@ -12,10 +12,97 @@ import java.util.stream.Stream;
 
 import static com.spring5microservices.common.PizzaEnum.CARBONARA;
 import static com.spring5microservices.common.PizzaEnum.MARGUERITA;
+import static com.spring5microservices.common.util.PredicateUtil.alwaysFalse;
+import static com.spring5microservices.common.util.PredicateUtil.alwaysTrue;
+import static com.spring5microservices.common.util.PredicateUtil.biAlwaysFalse;
+import static com.spring5microservices.common.util.PredicateUtil.biAlwaysTrue;
+import static com.spring5microservices.common.util.PredicateUtil.distinctByKey;
 import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PredicateUtilTest {
+
+    static Stream<Arguments> alwaysFalseTestCases() {
+        PizzaDto carbonara = new PizzaDto(CARBONARA.getInternalPropertyValue(), 5D);
+        return Stream.of(
+                //@formatter:off
+                //            sourceInstance,     expectedResult
+                Arguments.of( "noMatterString",   false ),
+                Arguments.of( 12,                 false ),
+                Arguments.of( carbonara,          false )
+        ); //@formatter:on
+    }
+
+    @ParameterizedTest
+    @MethodSource("alwaysFalseTestCases")
+    @DisplayName("alwaysFalse: test cases")
+    public <T> void alwaysFalse_testCases(T sourceInstance,
+                                          boolean expectedResult) {
+        assertEquals(expectedResult, alwaysFalse().test(sourceInstance));
+    }
+
+
+    static Stream<Arguments> alwaysTrueTestCases() {
+        PizzaDto carbonara = new PizzaDto(CARBONARA.getInternalPropertyValue(), 5D);
+        return Stream.of(
+                //@formatter:off
+                //            sourceInstance,     expectedResult
+                Arguments.of( "noMatterString",   true ),
+                Arguments.of( 12,                 true ),
+                Arguments.of( carbonara,          true )
+        ); //@formatter:on
+    }
+
+    @ParameterizedTest
+    @MethodSource("alwaysTrueTestCases")
+    @DisplayName("alwaysTrue: test cases")
+    public <T> void alwaysTrue_testCases(T sourceInstance,
+                                         boolean expectedResult) {
+        assertEquals(expectedResult, alwaysTrue().test(sourceInstance));
+    }
+
+
+    static Stream<Arguments> biAlwaysFalseTestCases() {
+        PizzaDto carbonara = new PizzaDto(CARBONARA.getInternalPropertyValue(), 5D);
+        return Stream.of(
+                //@formatter:off
+                //            firstSourceInstance,   secondSourceInstance,   expectedResult
+                Arguments.of( "noMatterString",      11,                     false ),
+                Arguments.of( 12,                    54L,                    false ),
+                Arguments.of( carbonara,             Boolean.TRUE,           false )
+        ); //@formatter:on
+    }
+
+    @ParameterizedTest
+    @MethodSource("biAlwaysFalseTestCases")
+    @DisplayName("biAlwaysFalse: test cases")
+    public <T, E> void biAlwaysFalse_testCases(T firstSourceInstance,
+                                               E secondSourceInstance,
+                                               boolean expectedResult) {
+        assertEquals(expectedResult, biAlwaysFalse().test(firstSourceInstance, secondSourceInstance));
+    }
+
+
+    static Stream<Arguments> biAlwaysTrueTestCases() {
+        PizzaDto carbonara = new PizzaDto(CARBONARA.getInternalPropertyValue(), 5D);
+        return Stream.of(
+                //@formatter:off
+                //            firstSourceInstance,   secondSourceInstance,   expectedResult
+                Arguments.of( "noMatterString",      11,                     true ),
+                Arguments.of( 12,                    54L,                    true ),
+                Arguments.of( carbonara,             Boolean.TRUE,           true )
+        ); //@formatter:on
+    }
+
+    @ParameterizedTest
+    @MethodSource("biAlwaysTrueTestCases")
+    @DisplayName("biAlwaysTrue: test cases")
+    public <T, E> void biAlwaysTrue_testCases(T firstSourceInstance,
+                                               E secondSourceInstance,
+                                               boolean expectedResult) {
+        assertEquals(expectedResult, biAlwaysTrue().test(firstSourceInstance, secondSourceInstance));
+    }
+
 
     static Stream<Arguments> distinctByKeyTestCases() {
         PizzaDto carbonaraCheap = new PizzaDto(CARBONARA.getInternalPropertyValue(), 5D);
@@ -26,7 +113,7 @@ public class PredicateUtilTest {
         Function<PizzaDto, Double> getCost = PizzaDto::getCost;
         return Stream.of(
                 //@formatter:off
-                //            initialCollection,                            keyExtractor,   expectedResult
+                //            initialCollection,                             keyExtractor,   expectedResult
                 Arguments.of( List.of(),                                     getName,        List.of() ),
                 Arguments.of( List.of(carbonaraCheap, carbonaraExpense),     getName,        List.of(carbonaraCheap) ),
                 Arguments.of( List.of(carbonaraCheap, margheritaCheap),      getName,        List.of(carbonaraCheap, margheritaCheap) ),
@@ -38,8 +125,12 @@ public class PredicateUtilTest {
     @ParameterizedTest
     @MethodSource("distinctByKeyTestCases")
     @DisplayName("distinctByKey: test cases")
-    public void distinctByKey_testCases(List<PizzaDto> initialCollection, Function<PizzaDto, String> keyExtractor, List<PizzaDto> expectedResult) {
-        List<PizzaDto> distinctCollection = initialCollection.stream().filter(PredicateUtil.distinctByKey(keyExtractor)).collect(toList());
+    public void distinctByKey_testCases(List<PizzaDto> initialCollection,
+                                        Function<PizzaDto, String> keyExtractor,
+                                        List<PizzaDto> expectedResult) {
+        List<PizzaDto> distinctCollection = initialCollection.stream()
+                .filter(distinctByKey(keyExtractor))
+                .collect(toList());
         assertEquals(expectedResult, distinctCollection);
     }
 
